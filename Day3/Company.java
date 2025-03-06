66feb0c2f44686c0e328f252e14862f5038d740e
mahalakshmi2007 <msrimahalakshmi543@gmail.com>
2025-03-06 15:24:19 +0530

    Create Company.java

---
+++ Day3/Company.java
@@ -0,0 +1,28 @@
+class Company {
+    static String companyName = "TechCorp";
+    String employeeName;
+
+    Company(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    void displayDetails() {
+        System.out.println("Company: " + companyName);
+        System.out.println("Employee: " + employeeName);
+    }
+
+    public static void main(String[] args) {
+        Company emp1 = new Company("Alice");
+        Company emp2 = new Company("Bob");
+
+        emp1.displayDetails();
+        emp2.displayDetails();
+
+        companyName = "InnovateX";
+
+        Company emp3 = new Company("Charlie");
+        emp3.displayDetails();
+        emp1.displayDetails();
+        emp2.displayDetails();
+    }
+}
